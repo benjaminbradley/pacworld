c5d4733c5f922f0c20004a94905a56420e930761
benjaminbradley <benjamin@gaiahost.coop>
2013-09-09 01:56:08 -0500

    added a straight-line swirl appearance type (look)

--- swirl.py
+++ swirl.py
@@ -1,10 +1,15 @@
 import pygame
 from pygame import *
 import logging
+import random
 
 import effect
 from effect import Effect
 import colors
+
+LOOK_CIRCLE = 1
+LOOK_LINE = 2
+LOOKS = [LOOK_CIRCLE, LOOK_LINE]
 
 class Swirl(sprite.Sprite):
 	
@@ -13,6 +18,7 @@ class Swirl(sprite.Sprite):
 		super(Swirl, self).__init__()
 		
 		self.effect_type = effect_type
+		self.look = LOOKS[random.randint(0,len(LOOKS)-1)]
 	
 	def activate(self, shape):
 		shape.effects[self.effect_type] = Effect(self.effect_type)
@@ -25,9 +31,18 @@ class Swirl(sprite.Sprite):
 		else:
 			color = colors.WHITE
 		if self.effect_type == effect.BURST_EFFECT:
-			# draw a tiny circle
-			radius = 3
-			pygame.draw.circle(image, color, position, radius, lineWidth)
+			if self.look == LOOK_CIRCLE:
+				# draw a tiny circle
+				radius = 3
+				pygame.draw.circle(image, color, position, radius, lineWidth)
+			elif self.look == LOOK_LINE:
+				# draw a straight line
+				length = 4
+				start_pos = (position[0] - length/2, position[1])
+				end_pos = (position[0] + length/2, position[1])
+				lineWidth = 2
+				pygame.draw.line(image, color, start_pos, end_pos, lineWidth)
+				
 		elif self.type == TRANSFER_EFFECT:
 			final_radius = image.get_width()
 			midpoint = self.animate_numframes / 2
