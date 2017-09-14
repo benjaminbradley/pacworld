3cc8238cca758f2939f02b494e8a900bf8565671
Benjamin Bradley <benjamin@wegeetokt>
2017-09-13 21:54:06 -0500

    remove duplicate code

--- swirl.py
+++ swirl.py
@@ -43,30 +43,6 @@ class Swirl(sprite.Sprite):
 				lineWidth = 2
 				pygame.draw.line(image, color, start_pos, end_pos, lineWidth)
 				
-		elif self.type == TRANSFER_EFFECT:
-			final_radius = image.get_width()
-			midpoint = self.animate_numframes / 2
-			if self.animate_frame < midpoint:
-				# first half of animation - getting bigger
-				burst_radius = int(float(self.animate_frame) / float(midpoint) * final_radius)
-			else:
-				# second half of animation - getting smaller
-				burst_radius = int(float(self.animate_numframes - self.animate_frame) / float(midpoint) * final_radius)
-				#if burst_radius < 0: burst_radius = 0
-
-			logging.debug("drawing transfer effect with burst_radius = {0}".format(burst_radius))
-			if burst_radius < lineWidth: lineWidth = burst_radius
-			gradpercent = float(self.animate_frame) / float(self.animate_numframes)
-			grad_color = [int(gradpercent*c) for c in colors.PINK]
-
-			#TODO: move it around!
-			origin_x = image.get_width() / 2
-			origin_y = image.get_height() / 2
-			#target_x = self.target.rect.centerx
-			#target_y = self.target.rect.centery
-			#curx = origin_x + (gradpercent * (target_x - origin_x))
-			#cury = origin_y + (gradpercent * (target_y - origin_y))
-			pygame.draw.circle(image, grad_color, (origin_x,origin_y), burst_radius, 0)
 		else:
 			logger.critical("unknown effect type: {0}".format(self.type))
 
