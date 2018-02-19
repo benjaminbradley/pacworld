98316f8955d45a5fc3498e8ed7b59fedf3e34ff3
Benjamin Bradley <benjamin@wegeekout.com>
2018-02-18 19:01:43 -0600

    reduce sound level for non-player shapes (more than offscreen, but below player's sounds)

--- pacdefs.py
+++ pacdefs.py
@@ -46,7 +46,7 @@ WALL_LINE_WIDTH = 8  # pixel width for drawing
 
 #sound
 NEARBY_SOUND_PERCENT = 0.2
-ONSCREEN_SOUND_PERCENT = 0.8
+ONSCREEN_SOUND_PERCENT = 0.5
 
 def opposite_side(side):
   # assumes an even number of sides
