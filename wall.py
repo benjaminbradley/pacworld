848d2b0b1c428a93c6a7bc22879f31b66e34988b
Benjamin Bradley <benjamin@wegeekout.com>
2018-05-07 22:21:09 -0500

    add unique IDs and to-string method for Walls

--- wall.py
+++ wall.py
@@ -7,10 +7,15 @@ import colors
 
 # The class for a blocking wall
 class Wall(sprite.Sprite):
+  wallId_autoIncrement = 0
 
   def __init__(self, mapSize, point1, point2):
     # Initialize the sprite base class
     super(Wall, self).__init__()
+
+    # assign a unique ID
+    self.id = Wall.wallId_autoIncrement
+    Wall.wallId_autoIncrement += 1
 
     # Get the display size for working out collisions later
     self.mapSize = mapSize
@@ -30,8 +35,15 @@ class Wall(sprite.Sprite):
     # grab a bitmask for collision detection
     self.mask = pygame.mask.from_surface(self.image)
     #print "DEBUG: wall mask is {0}".format(self.mask)
+    self.p1 = point1
+    self.p2 = point2
 
   def draw(self, display):
     # Draw the background to the display that has been passed in
     display.blit(self.image, (0,0))
-        
+
+  def __str__(self):
+    return "<Wall#{}:{} to {}>".format(self.id, self.p1, self.p2)
+
+  def __repr__(self):
+    return self.__str__()
